c950d37c13ffe0d8087d818c2ec9fc82565b747d
lkhamvongsa <lucie.khamvongsa@gmail.com>
2024-09-27 12:23:57 +0200

    Update config.js

--- linux_basics_session01/config.js
+++ linux_basics_session01/config.js
@@ -18,7 +18,7 @@ export const config = {
 	description: "IFB Scenario 1",
 	tags: ["unix", "shell", "terminal"],
 	tools: ["ls", "date"],
-	difficulty: ["belt yellow", "belt orange"],
+	difficulty: ["yellow belt", "orange belt"],
 	steps: [
 		{ name: "Basics of the Unix command line interface", component: Step0 },
 		{ name: "Unix & Command lines", component: Step1 },
