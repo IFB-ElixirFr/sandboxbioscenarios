cc3a44aa37d2d1741f60ff848b856e6c11cb73af
lkhamvongsa <lucie.khamvongsa@gmail.com>
2024-09-27 12:25:25 +0200

    Update config.js

--- linux_basics_session04/config.js
+++ linux_basics_session04/config.js
@@ -19,7 +19,7 @@ export const config = {
 	description: "IFB Scenario 4",
 	tags: ["unix", "shell", "terminal"],
 	tools: ["ls", "date"],
-	difficulty: ["belt blue"],
+	difficulty: ["blue belt"],
 	steps: [
 		{ name: "Combining bash commands", component: Step0 },
 		{ name: "Output of a Unix command", component: Step1 },
